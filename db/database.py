0111141dcc25d44cb49e7d81cacbb9c40471b590
AyalaFridman <a0556702870@gmail.com>
2025-02-11 10:59:07 +0200

    change database

--- db/database.py
+++ db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # קריאה למתני סביבה, ב-Railway המשתנה יהיה DATABASE_URL
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:HwJMBnyVDjTDjoNVmdyfBUPqPMODMLCw@postgres.railway.internal:5432/railway"
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL must be set in the environment variables")
